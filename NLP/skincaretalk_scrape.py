bbc3252107625d3daba08083a4c90a9917199779
David Wallwin <david.wallwin@gmail.com>
2017-09-10 23:24:30 +0100

    updates

--- NLP/skincaretalk_scrape.py
+++ NLP/skincaretalk_scrape.py
@@ -1,37 +1,37 @@
-
-
 import requests
-from requests_ntlm import HttpNtlmAuth
+#from requests_ntlm import HttpNtlmAuth
 import pickle
-
+import sys
 from bs4 import BeautifulSoup
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 maxPages = 1
 reviews = []
 baseurl = 'http://www.skincaretalk.com/{}'
-skincareviewurl = 'http://www.skincaretalk.com/forum.php'
+skincareviewurl = baseurl.format('forumdisplay.php/5-Basic-Skin-Care/page{}')
 skincareFileName = 'skincaretalk.pickle'
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:39.0) Gecko/20100101 Firefox/39.0'}
 
 def scrapeReviewsSkinCareTalk(pageNo, readThread = False):
-    url = skincareviewurl #.format(pageNo)
+    url = skincareviewurl.format(pageNo)
     print("calling:",url)
-    #requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-    response = requests.get(url)
-    with open('dump.txt','w') as f:
-        f.write(response.text)
-    return
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+    response = requests.get(url, headers=headers, verify=False)
     soup = BeautifulSoup(response.text,'lxml')
     for link in soup.findAll('a',attrs={'class':'title'}):     
-        print('found title')
         href = link.get('href')
         if href and readThread:
-            print(link.text) 
-            print("calling inner:",baseSkincareTalkUrl.format(href))
-            innerResponse = requests.get(baseSkincareTalkUrl.format(href),verify=False)
+            #print(link.text) 
+            print("calling inner:",baseurl.format(href))
+            innerResponse = requests.get(baseurl.format(href),headers=headers,verify=False)
             soup = BeautifulSoup(innerResponse.text,'lxml')
-            for divInner in soup.findAll('div',attrs={'class': 'content'}):
-                reviews.append(divInner.text)
+            text = ""
+            for divInner in soup.findAll('blockquote',attrs={'class': 'postcontent'}):
+                if text == "":
+                    text = "post:{} ".format(divInner.text)
+                else:
+                    text += "reply: {}".format(divInner.text)
+            reviews.append(text)
     if pageNo < maxPages:
             scrapeReviewsSkinCareTalk(pageNo = pageNo + 1)
     with open(skincareFileName,'wb') as f:
@@ -49,8 +49,16 @@ def getAllReviews(reload = False):
 
 getAllReviews(True)
 
-for review in reviews:
-    print(review)
+with open('dump.txt','w') as f:
+    for review in reviews:
+        f.write(review)
+        f.write("*"*50)
+
+
+# for review in reviews:
+#     print(review)
+
+
 
 print('found these reviews:',len(reviews))
 
---
+++ ScratchPad.py
@@ -0,0 +1,11 @@
+mydict = {}
+
+somevalues = (1,2,3,4,5)
+
+mydict[0] = somevalues
+
+print(mydict)
+
+for (i,j) in mydict.items():
+    print(i,j)
+
