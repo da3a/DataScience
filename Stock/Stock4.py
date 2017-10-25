3d08c54847050715e6eff5c78cb8f53bd1800e79
Wallwin <David.Wallwin@Lubrizol.com>
2017-10-25 16:52:54 +0100

    update

--- Stock/Stock4.py
+++ Stock/Stock4.py
@@ -9,20 +9,30 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 from sklearn import datasets, linear_model
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:39.0) Gecko/20100101 Firefox/39.0'}
+MARKET = "nasdaq"
+MARKETDATA = '^NDX.csv'
 
 def scrape_tickers(pageNo):
     print("scrape_tickers")
-    response = requests.get('http://www.londonstockexchange.com/exchange/prices-and-markets/stocks/indices/summary/summary-indices-constituents.html?index=ASX&page={}'.format(pageNo))
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+    response = requests.get('http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NASDAQ&page={}'.format(pageNo), headers=headers, verify=False)
     soup = bs.BeautifulSoup(response.text,'lxml')
-    table = soup.find('table',{'class','table_dati'})
+    # with open('debug.txt', 'a', encoding='utf-8') as the_file:
+    #     the_file.write(response.text)
+    table = soup.find('table',id='CompanylistResults')
     tickers = []
     for row in table.findAll('tr')[1:]:
-        ticker = row.findAll('td')[0].text
-        tickers.append(ticker)
+        if len(row.findAll('td')) > 1:
+            ticker = row.findAll('td')[1].text.rstrip().lstrip()
+            print('ticker found:{}'.format(ticker))
+            tickers.append(ticker)
     return tickers
 
-def save_ftse_tickers():
-    print("save_ftse_tickers")
+def save_tickers():
+    print("save_tickers")
     tickers = []
     cnt = 0
     pageTotal = 32
@@ -31,38 +41,38 @@ def save_ftse_tickers():
         pagedTickers = scrape_tickers(cnt)
         tickers.extend(pagedTickers)
     
-    with open('ftse.pickle','wb') as f:
+    with open('{}.pickle'.format(MARKET),'wb') as f:
         pickle.dump(tickers,f)
     return tickers
 
-def get_ftse_tickers(reload_tickers=False):
-    print("get_ftse_tickers")
+def get_tickers(reload_tickers=False):
+    print("get_tickers")
     if reload_tickers:
-        tickers = save_ftse_tickers()
+        tickers = save_tickers()
     else:
-        with open('ftse.pickle','rb') as f:
+        with open('{}.pickle'.format(MARKET),'rb') as f:
             tickers = pickle.load(f)
     return tickers
 
 def get_ticker_data_from_web(ticker, startTime, endTime):
     try:
         df = web.DataReader(ticker,'google', startTime, endTime)
-        df.to_csv('ftse/{}.csv'.format(ticker))
+        df.to_csv('{}/{}.csv'.format(MARKET,ticker))
         return df
     except:
         return pd.DataFrame({'empty' : []})
 
 def get_ticker_data(ticker,startTime,endTime):
     print('get_ticker_data {} {} {}'.format(ticker, startTime, endTime))
-    if not os.path.exists('ftse'):
-        os.makedirs('ftse')
+    if not os.path.exists(MARKET):
+        os.makedirs(MARKET)
     try:
-        return readFile('ftse/{}.csv'.format(ticker))
+        return readFile('{}/{}.csv'.format(MARKET,ticker))
     except:
         print('ticker file not found will reload')
         df = get_ticker_data_from_web(ticker, startTime, endTime)
         if not df.empty:
-            return readFile('ftse/{}.csv'.format(ticker))
+            return readFile('{}/{}.csv'.format(MARKET,ticker))
         else:
             return df
 
@@ -74,13 +84,13 @@ def readFile(file_name):
     data.index = data['Date']
     return data
 
-#def getCoef(data):
-# get_ftse_tickers(reload_tickers=True)
+# tickers = get_tickers(reload_tickers=True)
+# print(tickers)
 # sys.exit(0)
 
 startTime = dt.datetime(2017,4,1)
 endTime = dt.datetime(2017,10,1)
-tickers = get_ftse_tickers(reload_tickers=False)
+tickers = get_tickers(reload_tickers=False)
 
 # for ticker in tickers:
 #     print('getting ticker data for:{}'.format(ticker))
@@ -95,11 +105,11 @@ tickers = get_ftse_tickers(reload_tickers=False)
 
 #sys.exit(0)
 
-ftsedata = pd.read_csv('ftse/^FTSE_1.csv',sep=',',usecols=[0,5],names=['Date','Price'],header=1)
-ftsereturns = np.array(ftsedata['Price'][1:],np.float)/np.array(ftsedata['Price'][:-1],np.float)-1
-ftsedata['Returns'] = np.append(ftsereturns,np.nan)
-ftsedata.index = ftsedata['Date']
-#print(ftse.describe())
+marketdata = pd.read_csv(MARKETDATA,sep=',',usecols=[0,5],names=['Date','Price'],header=1)
+marketreturns = np.array(marketdata['Price'][1:],np.float)/np.array(marketdata['Price'][:-1],np.float)-1
+marketdata['Returns'] = np.append(marketreturns,np.nan)
+marketdata.index = marketdata['Date']
+
 
 def plot_figure(tickers):
         fig, ax = plt.subplots(nrows=5, ncols=5)
@@ -110,7 +120,10 @@ def plot_figure(tickers):
             df = get_ticker_data(ticker, startTime, endTime)
             if df.empty:
                 continue
-            modeldata = pd.merge(ftsedata,df,how='inner',on=['Date'])
+            if len(df) < 2:
+                continue
+
+            modeldata = pd.merge(marketdata,df,how='inner',on=['Date'])
             print(modeldata)
             year_xData = modeldata['Returns_x'][0:-1].values.reshape(-1,1)
             year_yData = modeldata['Returns_y'][0:-1]
@@ -118,7 +131,6 @@ def plot_figure(tickers):
             halfyear_xData = modeldata['Returns_x'][-180:-1].values.reshape(-1,1)
             month_xData = modeldata['Returns_x'][-30:-1].values.reshape(-1,1)
             month_yData = modeldata['Returns_y'][-30:-1]
-
             year_model = linear_model.LinearRegression()
             halfyear_model = linear_model.LinearRegression()
             month_model = linear_model.LinearRegression()
@@ -126,25 +138,21 @@ def plot_figure(tickers):
             halfyear_model.fit(year_xData,year_yData)
             month_model.fit(month_xData,month_yData)
             print('ticker:{} slope is{}'.format(ticker,year_model.coef_))
-            ax = plt.subplot(5,5, ctr)
-            ax.set_title(ticker + " " + str(year_model.coef_))
-            plt.scatter(month_xData,month_yData, color='black')   
-            plt.plot(year_xData,year_model.predict(year_xData),color='blue',linewidth=1)
-            plt.plot(halfyear_xData,halfyear_model.predict(halfyear_xData),color='green',linewidth=1)
-            plt.plot(month_xData,month_model.predict(month_xData),color='red',linewidth=1)
-            plt.axis([min(modeldata['Returns_x']),max(modeldata['Returns_x']),min(modeldata['Returns_y']),max(modeldata['Returns_y'])])
-            ctr = ctr+1
+            if year_model.coef_ > 1:
+                ax = plt.subplot(5,5, ctr)
+                ax.set_title(ticker + " " + str(year_model.coef_))
+                plt.scatter(month_xData,month_yData, color='black')   
+                plt.plot(year_xData,year_model.predict(year_xData),color='blue',linewidth=1)
+                plt.plot(halfyear_xData,halfyear_model.predict(halfyear_xData),color='green',linewidth=1)
+                plt.plot(month_xData,month_model.predict(month_xData),color='red',linewidth=1)
+                plt.axis([min(modeldata['Returns_x']),max(modeldata['Returns_x']),min(modeldata['Returns_y']),max(modeldata['Returns_y'])])
+                ctr = ctr+1
 
         plt.show()
+
 
 for i in range(1, len(tickers)-26, 26):
     plot_figure(tickers[i:i+26])
 
-# for ticker in tickers:
-#     try:
-#         df = readFile('ftse/{}.csv'.format(ticker))
-#         df.plot()
-#         plt.show()
-#     except:
-#         print('cannot load data for {}'.format(ticker))
+
     
